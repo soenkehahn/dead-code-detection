ceddfe0859f9ec61c19ee56a8845d2c3a4c7d4cf
Sönke Hahn <soenke.hahn@zalora.com>
2015-08-12 19:19:29 +0800

    wip

--- src/Parse.hs
+++ src/Parse.hs
@@ -4,12 +4,18 @@ module Parse where
 import           GHC
 import           GHC.Paths (libdir)
 
-parse :: FilePath -> IO Ast
-parse file = runGhc (Just libdir) $ do
-  getSessionDynFlags >>= setSessionDynFlags
-  guessTarget file Nothing >>= setTargets . pure
-  [mod] <- depanal [] False
-  renamed <- tm_renamed_source <$> (parseModule mod >>= typecheckModule)
-  undefined
+parse :: FilePath -> IO (Either String Ast)
+parse file =
+  handleSourceError foo $
+  runGhc (Just libdir) $ do
+    getSessionDynFlags >>= setSessionDynFlags
+    guessTarget file Nothing >>= setTargets . pure
+    [mod] <- depanal [] False
+    renamed <- tm_renamed_source <$> (parseModule mod >>= typecheckModule)
+    error "not in scope: bar"
+    maybe (error "fixme") (\ (a, _, _, _) -> return a) renamed
 
-type Ast = ()
+type Ast = HsGroup Name
+
+handleSourceError :: IO a -> IO a
+handleSourceError = id
--- test/ParseSpec.hs
+++ test/ParseSpec.hs
@@ -2,19 +2,33 @@
 
 module ParseSpec where
 
+import           Control.Exception
 import           Data.String.Interpolate
 import           Data.String.Interpolate.Util
+import           Outputable
 import           Test.Hspec
 import           Test.Mockery.Directory
 
 import           Parse
 
 spec = do
-  describe "readAst" $ do
-    it "fixme" $ do
+  describe "parse" $ do
+    it "parses a simple module" $ do
       inTempDirectory $ do
-        writeFile "Test.hs" $ unindent [i|
+        writeFile "Foo.hs" $ unindent [i|
           module Foo where
+          foo = 3 -- bar
         |]
-        ast <- parse "Test.hs"
-        pending
+        ast <- parse "Foo.hs"
+        showAst ast `shouldBe` "foo = 3"
+
+    it "handles an invalid module gracefully" $ do
+      inTempDirectory $ do
+        writeFile "Foo.hs" $ unindent [i|
+          module Foo where
+          foo = bar
+        |]
+        parse "Foo.hs" `shouldThrow` (== ErrorCall "not in scope: bar")
+
+showAst :: Ast -> String
+showAst = showSDocUnsafe . ppr
